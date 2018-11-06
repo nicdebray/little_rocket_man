20bd52fd92bffd8eae42ea0aaac34687488f950b
Wouter Verhaeghe <wouter_verhaeghe@hotmail.com>
2018-11-06 10:55:18 +0100

    actions renter controller added

--- app/controllers/renters_controller.rb
+++ app/controllers/renters_controller.rb
@@ -1,8 +1,17 @@
 class RentersController < ApplicationController
+  before_action :set_renter, only: [:show, :edit, :update]
+
   def new
+    @renter = Renter.new
   end
 
   def create
+    @renter = Renter.new(renter_params)
+    if @renter.save
+      redirect_to renter_path(@renter)
+    else
+      render: new
+    end
   end
 
   def show
@@ -12,5 +21,22 @@ class RentersController < ApplicationController
   end
 
   def update
+    if @renter.update(renter_params)
+      redirect_to renter_path(@renter)
+    else
+      render: new
+    end
+  end
+
+  private
+
+  def set_renter
+    @renter = Renter.find(params[:id])
+  end
+
+  def renter_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:renter).permit(:first_name, :last_name, :country, :user_id)
   end
 end
