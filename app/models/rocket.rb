1d6ce7704964b5617ae02a714b35f2b28faa5394
Nicolas Debray <nicdebray@gmail.com>
2018-11-08 15:15:30 +0100

    photo uploader working

--- app/models/rocket.rb
+++ app/models/rocket.rb
@@ -2,6 +2,8 @@ class Rocket < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+  mount_uploader :photo, PhotoUploader
+
   validates :name, presence: true
   validates :capacity, presence: true, numericality: {greater_than: 0}
   validates :price, presence: true, numericality: {greater_than: 0}
